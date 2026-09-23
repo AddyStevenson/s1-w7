791e125219e68f4f19cb32db180e4a0d7065ff9b
Addy Stevenson <addystevenson748@gmail.com>
2026-09-23 12:11:25 -0700

    ww

--- WeightMain.java
+++ WeightMain.java
@@ -2,11 +2,11 @@ public class WeightMain{
     public static void main(String[] args) {
         Weight w1 = new Weight(10, 5);
         Weight w2 = new Weight(5,10);
-        boolean b1 = w1.isHeavier(w1);
+        boolean b1 = w1.isHeavier(w2);
         System.out.println(b1);
 
         Weight w3 = w1.multiple(2);
-        w2.print();
+        w3.print();
     }
 
 
