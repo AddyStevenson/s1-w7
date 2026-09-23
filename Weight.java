bcbe76707665f38cbbbac13c30b64a4d94e04a42
Addy Stevenson <addystevenson748@gmail.com>
2026-09-23 12:02:19 -0700

    ii

--- Weight.java
+++ Weight.java
@@ -16,13 +16,29 @@ public Weight(int pounds, int ounces){
     if(pounds > 0)
         this.pounds = pounds;
 }
+public int totalOunces(){
+    return this.ounces + 16 * this.pounds;
+}
 
 public boolean isHeavier(Weight other){
-    int totalW = this.ounces + 16 * this.pounds;
-    int otherW = other.ounces + 16 * other.pounds;
+    int totalW = totalOunces();
+    int otherW = other.totalOunces();
     if(totalW > otherW)
         return true;
     else 
         return false;
 }
- 
+public Weight multiple(int scale){
+     int total = this.totalOunces() * scale; 
+     int p = total / 16;
+     int o = total % 16;
+     return new Weight(o, p);
+}
+
+public void print(){
+    System.out.println(this.pounds + "pounds, " + this.ounces + " ounces");
+}
+
+
+
+}
